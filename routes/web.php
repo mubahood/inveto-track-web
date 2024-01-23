053c149c6690448d79fb1d1ca46bc4e452424f49
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-01-23 19:14:49 +0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 /* Route::get('/', function () {
-    return view('welcome');
+    return die('welcome');
 });
-*/
+Route::get('/home', function () {
+    return die('welcome home');
+});
+ */
