9677dbb3c85de7aeb046b71e7624fbbe89f49f94
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-03-25 23:20:27 +0300

    Refactor Gen.php: Add new static methods for fetching and deleting data

--- app/Models/Gen.php
+++ app/Models/Gen.php
@@ -34,14 +34,18 @@ class Gen extends Model
 
                 $variables
 
-                static fromJson(dynamic m) {
-                    $this->class_name item =  $this->class_name();
-                        if (m == null) {
-                            return item;
+        
+                static Future&ltList&lt{$this->class_name}&gt&gt get_items(
+                    {String where = ""}) async {
+                        List&lt{$this->class_name}&gt items = await get_local_items();
+                        if (items.isEmpty) {
+                            await get_online_items();
+                            items = await get_local_items();
+                        } else {
+                            get_online_items();
                         }
-                    $fromJson
-                    return item;
-            }
+                    return items;
+                }
 
 
         static Future&ltList&lt{$this->class_name}&gt&gt get_local_items({
@@ -72,6 +76,174 @@ class Gen extends Model
             });
             return data;
             } 
+
+
+            static fromJson(dynamic m) {
+                $this->class_name item =  $this->class_name();
+                    if (m == null) {
+                        return item;
+                    }
+                $fromJson
+                return item;
+            }
+
+
+             
+
+            static Future&ltvoid&gt get_online_items() async {
+                if (!(await Utils.isConnected())) {
+                  return;
+                }
+                ResponseModel? resp = null;
+                try {
+                  resp = ResponseModel(await Utils.http_get('api/\$end_point', {}));
+                } catch (e) {
+                  resp = null;
+                }
+                if (resp == null) {
+                  print("FAILED TO FETCH DATA");
+                  return;
+                }
+            
+                if (resp.code != 1) {
+                  Utils.toast(
+                    "Failed to fetch \$tableName data. \${resp . message}",
+                    c: Colors.red,
+                  );
+                  return;
+                }
+            
+                try {
+                  await delete_all();
+                } catch (e) {
+                  Utils.toast("Failed to delete all \$tableName data. \${e . toString()}",
+                      c: Colors.red);
+                }
+            
+                if (!(resp.data.runtimeType.toString().toLowerCase().contains('list'))) {
+                  Utils.toast("Failed to fetch \$tableName data. \${resp . data}",
+                      c: Colors.red);
+                  return;
+                }
+            
+                //database db
+                Database db = await Utils.getDb();
+                //init table
+                String tamp_resp = await initTable(db);
+            
+                if (!tamp_resp.isEmpty) {
+                  Utils.toast("Failed to init table \$tableName. \$tamp_resp", c: Colors.red);
+                  return;
+                }
+                await db.transaction((transaction) async {
+                  var batch = transaction.batch();
+                  for (var x in resp?.data) {
+                    {$this->class_name} item = {$this->class_name}.fromJson(x);
+                    try {
+                      batch.insert(tableName, item.toJson(),
+                          conflictAlgorithm: ConflictAlgorithm.replace);
+                    } catch (e) {
+                      Utils.toast("Failed to save \$tableName data. \${e . toString()}",
+                          c: Colors.red);
+                    }
+                  }
+                  try {
+                    await batch.commit(continueOnError: true);
+                  } catch (e) {
+                    Utils.toast("Failed to commit \$tableName data. \${e . toString()}",
+                        c: Colors.red);
+                  }
+                });
+              }
+
+
+
+
+
+              static Future&ltString&gt delete_all() async {
+                Database db = await Utils.getDb();
+                if (!db.isOpen) {
+                  return 'Failed to open database.';
+                }
+                try {
+                  await db.delete(tableName);
+                  return '';
+                } catch (e) {
+                  return 'Failed to delete table because \${e.toString()}.';
+                }
+              }
+
+
+              Future&ltString&gt delete() async {
+                Database db = await Utils.getDb();
+                if (!db.isOpen) {
+                  return 'Failed to open database.';
+                }
+                try {
+                  await db.delete(tableName, where: 'id = ?', whereArgs: [id]);
+                  return '';
+                } catch (e) {
+                  return 'Failed to delete item because \${e.toString()}.';
+                }
+              }
+
+
+              static Future&ltString&gt delete_item(int id) async {
+                Database db = await Utils.getDb();
+                if (!db.isOpen) {
+                  return 'Failed to open database.';
+                }
+                try {
+                  await db.delete(tableName, where: 'id = ?', whereArgs: [id]);
+                  return '';
+                } catch (e) {
+                  return 'Failed to delete item because \${e.toString()}.';
+                }
+              }
+
+
+              Future&ltString&gt save() async {
+                Database db = await Utils.getDb();
+                String table_results = await initTable(db);
+                if (table_results.isNotEmpty) {
+                  return table_results;
+                }
+                try {
+                  await db.insert(tableName, toJson(),
+                      conflictAlgorithm: ConflictAlgorithm.replace);
+                  return "";
+                } catch (e) {
+                  return 'Failed to save item because \${e.toString()}.';
+                }
+                return "";
+              }
+
+
+
+              static Future&ltString&gt initTable(Database db) async {
+                if (!db.isOpen) {
+                  Utils.toast('Failed to open database.', c: Colors.red);
+                  return 'Failed to open database.';
+                }
+                String sql = 'CREATE TABLE IF NOT EXISTS \${tableName} ('
+                     {$sqlTableVars}
+                    ')';
+                try {
+                  await db.execute(sql);
+                  return "";
+                } catch (e) {
+                  return 'Failed to create table because \${e . toString()}.';
+                }
+              } 
+
+              
+              toJson() {
+                return {
+                    $toJson
+                };
+              }
+            
+
         }
         </pre>
         EOT;
