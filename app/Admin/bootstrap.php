d1da1cdf6e6d66b9956dab03ee351e2ec932ca66
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-05-06 08:05:55 +0300

    as

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -20,10 +20,47 @@
 
 use App\Models\Utils;
 use Encore\Admin\Facades\Admin;
+
 Utils::importRecs();
+
+/* $msg = "Your ICT4Farmers vendor registration request has been received, we are going to review and get back to you shortly.";
+Utils::send_sms([
+    'to' => $phone_number,
+    'message' => $msg
+]); */
+$u = Admin::user();
+$review_link = admin_url('system-users/' . $u->id . '/edit');
+$mail_body = <<<EOD
+                    <p>Dear Admin,</p>
+                    <p>New vendor registration request from {$u->name}.</p>
+                    <p>Business Name: {$u->business_name}</p>
+                    <p>Business Address: {$u->business_address}</p>
+                    <p>Business Phone Number: {$u->business_phone_number}</p>
+                    <p>Business Category: {$u->business_category}</p>
+                    <p>Location: {$u->location_id}</p>
+                    <p>Phone Number: {$u->phone_number}</p>
+                    <p>Email: {$u->email}</p>
+                    <p>Click <a href="{$review_link}">here</a> to review this request.</p>
+                    <p>Thank you.</p>
+                EOD;
+$data['email'] = [
+    'mubahood360@gmail.com',
+];
+$date = date('Y-m-d');
+$data['subject'] = env('APP_NAME') . " - New Vendor Registration Request: ";
+$data['body'] = $mail_body;
+$data['data'] = $data['body'];
+$data['name'] = 'Admin';
+try {
+    Utils::mail_sender($data);
+    die('Sent');
+} catch (\Throwable $th) {
+    throw $th;
+}
+die('ok');
 
 Encore\Admin\Form::forget(['map', 'editor']);
 $u = Admin::user();
-if($u !=null){
+if ($u != null) {
     Utils::generate_dummy($u);
-} 
+}
